c9bc26d98407fe44ab052a7bd645469350dae8f9
Daniel V. Tan <danielvtan@gmail.com>
2013-08-06 17:21:43 +0800

    Merged onreadystatechange event call

--- js/Ajaxer.js
+++ js/Ajaxer.js
@@ -1,10 +1,6 @@
-
-
-var Ajaxer = new AJAXER();
-function AJAXER(){
-	
-	
-	this.getRequest = function() {
+var Ajaxer = new Ajaxer();
+function Ajaxer(){
+	this.getRequest = function(onComplete) {
 		var request;
 		if (window.XMLHttpRequest){
 			// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -13,33 +9,27 @@ function AJAXER(){
 			// code for IE6, IE5
 			request = new ActiveXObject("Microsoft.XMLHTTP");
 		}
-		return request;
-	}
-	this.get = function(url, onComplete){
-		var request = this.getRequest();
-		request.onreadystatechange = function(){
+        request.onreadystatechange = function(){
 			if (request.readyState == 4 && request.status == 200){
 				onComplete(request.responseText);
 			}
 		}
+		return request;
+	}
+	this.get = function(url, onComplete){
+		var request = this.getRequest(onComplete);
 		request.open("GET", url, true);
 		request.send();
 	}
 	this.post = function(url, dataObject, onComplete){
-		var request = this.getRequest();
-		request.onreadystatechange = function(){
-			if (request.readyState == 4 && request.status==200){
-				onComplete(request.responseText);
-			}
-		}
+		var request = this.getRequest(onComplete);
 		var queryString = "";
 		for(var i in dataObject) {
 			queryString += i + "=" + dataObject[i] +  "&";
-			
 		}
 		request.open("POST",url ,true);
 		request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 		request.send(queryString);
 	}
-	
+
 } 
